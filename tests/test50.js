d1a6338938aa327c3c56d805694b0cb5731f778f
John Binning <john.binning17@gmail.com>
2017-02-18 16:56:06 -0700

    add initial test1-4

---
+++ tests/test50.js
@@ -0,0 +1,41 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+
+var driver_chr = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+
+searchTest(driver_chr);
+
+
+function searchTest(driver) {
+  driver.get('https://dbull7.github.io/ideabox/');
+  driver.findElement(By.className('user-title')).sendKeys('plz');
+  driver.findElement(By.className('user-body')).sendKeys('work');
+  driver.findElement(By.className('save-button')).click();
+  driver.findElement(By.className('delete-button')).click();
+
+  driver.sleep(3000).then(function() {
+    driver.findElement(By.className('idea-list')).then(driver.findElement(By.tagName('article')).getText(function(text){
+    if (text !== 'work') {
+      console.log('test passed')
+    } else {
+      console.log('test failed damnit')
+    };
+
+
+    // driver.findElement(By.className('body-text')).then(function(text) {
+    //   if() {
+    //     console.log('Test passed');
+    //   } else {
+    //     console.log('Test failed');
+    //   }
+    // });
+  };
+
+  driver.quit();
+})
+}

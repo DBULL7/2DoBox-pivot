fa60a3084342bcf91e122971a2402a035203925a
Devon Bull <dbull@live.com>
2017-02-18 14:45:14 -0700

    first test functional, update readme

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 ##ideabox
 
-####A minty home for all your fresh ideas
-
-#####Chris Bandrowsky and Laura Turk. Project for Mod 1 of the FE program at Turing School for Software and Design.
-
-######Users can input their ideas by title and the ideas will persist in local storage. Change the rating of your idea from Swill to Plausible to Genius, and edit on the go! Search your ideas by title or body.
+## Hosted on [Github Pages](https://dbull7.github.io/2DoBox-pivot/)
--- tests/test1.js
+++ tests/test1.js
@@ -3,22 +3,29 @@ var webdriver = require('selenium-webdriver'),
     until = webdriver.until;
 
 
+var driver_fx = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+
 var driver_chr = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
 
 
-searchTest(driver_chr);
+makesCard(driver_fx);
+makesCard(driver_chr);
 
 
-function searchTest(driver) {
-  driver.get('http://www.google.com');
-  driver.findElement(By.name('q')).sendKeys('webdriver');
-  driver.findElement(By.name('btnG')).click();
+
+function makesCard(driver) {
+  driver.get('https://dbull7.github.io/2DoBox-pivot/');
+  driver.findElement(By.id('title-input')).sendKeys('please');
+  driver.findElement(By.id('content-input')).sendKeys('work');
+  driver.findElement(By.id('submit')).click();
 
   driver.sleep(3000).then(function() {
-    driver.getTitle().then(function(title) {
-      if(title === 'webdriver - Google Search') {
+    driver.findElement(By.className('entry-title')).getText().then(function(title) {
+      if(title === 'please') {
         console.log('Test passed');
       } else {
         console.log('Test failed');
